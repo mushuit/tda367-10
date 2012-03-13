484eb208d5b65a215723f27cd1337c538844ec47
Mushu <linus@mushu.it>
2012-03-13 12:53:20 +0100

    importerar slick-biblioteket för spel

---
+++ src/SimpleTest.java
@@ -0,0 +1,36 @@
+
+	import org.newdawn.slick.BasicGame;
+	import org.newdawn.slick.GameContainer;
+	import org.newdawn.slick.Graphics;
+	import org.newdawn.slick.SlickException;
+	import org.newdawn.slick.AppGameContainer;
+
+	public class SimpleTest extends BasicGame {
+
+	    public SimpleTest() {
+	        super("SimpleTest");
+	    }
+	    
+	    @Override
+	    public void init(GameContainer container) throws SlickException {}
+
+	    @Override
+	    public void update(GameContainer container, int delta)
+	            throws SlickException {}
+
+	    @Override
+	    public void render(GameContainer container, Graphics g)
+	            throws SlickException {
+	        g.drawString("Hello, Slick world!", 0, 100);
+	    }
+
+	    public static void main(String[] args) {
+	        try {
+	            AppGameContainer app = new AppGameContainer(new SimpleTest());
+	            app.start();
+	        } catch (SlickException e) {
+	            e.printStackTrace();
+	        }
+	    }
+	}
+
