7bddd745a03690fe77d83900bf293bb142f8fa37
Bosch <magnushuttu@gmail.com>
2012-03-13 15:22:30 +0100

    Updated

--- src/AnotherSimpleTest.java
+++ src/AnotherSimpleTest.java
@@ -24,97 +24,113 @@ public class AnotherSimpleTest implements Game{
 	String play;
 	boolean dead = false;
 	double poang = 0;
-	
+	boolean pause = false;
+
 	public AnotherSimpleTest(){
 		yPositions = new int[size*2];
 		xPositions = new int[size*2];
 		play = "You are still alive";
 	}
-	
-    public void init(GameContainer container) throws SlickException {
 
-    }
-    
-    public void update(GameContainer container, int delta)
-            throws SlickException {
-    	Input input = container.getInput();
-    	if(!dead)
-    	poang += 0.1;
-    	
-    	for(int i = 0; i < 20; i++){
-    		yPositions[i] = yPos+10-i;
-    		xPositions[i] = xPos+10-i;
-    	}
-    	if(input.isKeyDown(Input.KEY_DOWN)){
-    		yPos = yPos+movement;
-    	}
-    	if(input.isKeyDown(Input.KEY_UP)){
-    		yPos = yPos-movement;
-    	}
-    	if(input.isKeyDown(Input.KEY_RIGHT)){
-    		xPos = xPos+movement;
-    	}
-    	if(input.isKeyDown(Input.KEY_LEFT)){
-    		xPos = xPos-movement;
-    	}
-    	if(input.isKeyDown(Input.KEY_B)){
-    		xPos = 100;
-    		yPos = 100;
-    	}
-    	if(input.isKeyDown(Input.KEY_ENTER)){
-    		dead = false;
-    		play = "You are still alive";
-    	}
-    	yBossPos += movement;
-    	xBoss2Pos += movement;
-    	
-    	if(yBossPos > yMax){
-    		yBossPos = 0;
-    		xBossPos = (int)(Math.random()*xMax);
-    	}
-    	if(xBoss2Pos > xMax){
-    		xBoss2Pos = 0;
-    		yBoss2Pos = (int)(Math.random()*yMax);
-    	}
-    	for(int i = 0; i < 20; i++){
-    		if(yBossPos == yPositions[i]){
-    			if(xBossPos == xPositions[i]){
-    				play = "You died(Enter to restart)";
-    				dead = true;
-    			}
-    		
-    		}
-    	}
-    	for(int i = 0; i < 20; i++){
-    		if(xBoss2Pos == xPositions[i]){
-    			if(yBoss2Pos == yPositions[i]){
-    				play = "You died(Enter to restart)";
-    				dead = true;
-    			}
-    		
-    		}
-    	}
-    }
+	public void init(GameContainer container) throws SlickException {
 
-    public void render(GameContainer container, Graphics g)
-            throws SlickException {
-        g.setColor(Color.green);
-        g.drawString(play + ", poŠng: " + (int)poang, 0, 100);
-        if(!dead)
-        g.drawRect(xPos, yPos, size, size);
-        g.setColor(Color.red);
-        g.drawOval(xBossPos, yBossPos, 20, 20);
-        g.drawOval(xBoss2Pos, yBoss2Pos, 20, 20);
-        
-    }
-    
+	}
+
+	public void update(GameContainer container, int delta)
+			throws SlickException {
+			Input input = container.getInput();
+		if(input.isKeyPressed(input.KEY_P)){
+			pause = !pause;
+		}
+		if(!pause){
+			if(!dead)
+				poang += 0.1;
+
+			for(int i = 0; i < 20; i++){
+				yPositions[i] = yPos+10-i;
+				xPositions[i] = xPos+10-i;
+			}
+			if(input.isKeyDown(Input.KEY_DOWN)){
+				yPos = yPos+movement;
+			}
+			if(input.isKeyDown(Input.KEY_UP)){
+				yPos = yPos-movement;
+			}
+			if(input.isKeyDown(Input.KEY_RIGHT)){
+				xPos = xPos+movement;
+			}
+			if(input.isKeyDown(Input.KEY_LEFT)){
+				xPos = xPos-movement;
+			}
+			if(input.isKeyDown(Input.KEY_B)){
+				xPos = 100;
+				yPos = 100;
+			}
+			if(input.isKeyDown(Input.KEY_ENTER)){
+				dead = false;
+				play = "You are still alive";
+				poang = 0;
+			}
+			yBossPos += movement;
+			xBoss2Pos += movement;
+
+			if(yBossPos > yMax){
+				yBossPos = 0;
+				xBossPos = (int)(Math.random()*xMax);
+			}
+			if(xBoss2Pos > xMax){
+				xBoss2Pos = 0;
+				yBoss2Pos = (int)(Math.random()*yMax);
+			}
+			if(yPos > yMax){
+				yPos = 0;
+			} else if(yPos < 0){
+				yPos = yMax;
+			}
+			if(xPos > xMax){
+				xPos = 0;
+			} else if(xPos < 0){
+				xPos = xMax;
+			}
+			for(int i = 0; i < 20; i++){
+				if(yBossPos == yPositions[i]){
+					if(xBossPos == xPositions[i]){
+						play = "You died(Enter to restart)";
+						dead = true;
+					}
+				}
+			}
+			for(int i = 0; i < 20; i++){
+				if(xBoss2Pos == xPositions[i]){
+					if(yBoss2Pos == yPositions[i]){
+						play = "You died(Enter to restart)";
+						dead = true;
+					}
+				}
+			}
+		}
+	}
+
+	public void render(GameContainer container, Graphics g)
+			throws SlickException {
+		g.setColor(Color.green);
+		g.drawString(play + ", poŠng: " + (int)poang, 0, 100);
+		if(!dead)
+			g.drawRect(xPos, yPos, size, size);
+		g.setColor(Color.red);
+		g.drawOval(xBossPos, yBossPos, 20, 20, 20);
+		g.drawOval(xBossPos, yBossPos, 20, 20);
+		g.drawOval(xBoss2Pos, yBoss2Pos, 20, 20);
+
+	}
+
 	public static void main(String[] args) {
 		try {
-            AppGameContainer app = new AppGameContainer(new AnotherSimpleTest());
-            app.start();
-        } catch (SlickException e) {
-            e.printStackTrace();
-        }
+			AppGameContainer app = new AppGameContainer(new AnotherSimpleTest());
+			app.start();
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -126,5 +142,5 @@ public class AnotherSimpleTest implements Game{
 	public String getTitle() {
 		return ".:|BAWS|:.";
 	}
- 
+
 }
