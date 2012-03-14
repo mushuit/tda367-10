6e41a776785ca5c9b03153736ecb66c701b698fe
Bosch <magnushuttu@gmail.com>
2012-03-14 11:04:27 +0100

    Måste committa för att få tillbaka filerna -.-'

--- src/AnotherSimpleTest.java
+++ src/AnotherSimpleTest.java
@@ -26,10 +26,13 @@ public class AnotherSimpleTest implements Game{
 	int[] xBossPositions;
 	int[] yBoss2Positions;
 	int[] xBoss2Positions;
+	int[] yShieldPositions;
+	int[] xShieldPositions;
 	String play;
 	boolean dead = false;
 	double poang = 0;
 	boolean pause = false;
+	boolean shield = false;
 
 	public AnotherSimpleTest(){
 		yPositions = new int[size*2];
@@ -58,13 +61,21 @@ public class AnotherSimpleTest implements Game{
 			for(int i = 0; i < 20; i++){
 				yPositions[i] = yPos+10-i;
 				xPositions[i] = xPos+10-i;
-				yBossPositions[i] = yBossPos+10-i;
-				xBossPositions[i] = xBossPos+10-i;
-				yBoss2Positions[i] = yBossPos+10-i;
-				xBoss2Positions[i] = xBossPos+10-i;
+			}
+			for(int i = 0; i < 40; i++){
+				yBossPositions[i] = yBossPos+20-i;
+				xBossPositions[i] = xBossPos+20-i;
+				yBoss2Positions[i] = yBossPos+20-i;
+				xBoss2Positions[i] = xBossPos+20-i;
+				yShieldPositions[i] = yPos+20-i;
+				xShieldPositions[i] = xPos+20-i;
 			}
 			if(input.isKeyDown(Input.KEY_DOWN)){
 				yPos = yPos+movement;
+			}
+			if(input.isKeyDown(Input.KEY_K)){
+				xBossPos = xPos;
+				yBoss2Pos = yPos;
 			}
 			if(input.isKeyDown(Input.KEY_UP)){
 				yPos = yPos-movement;
@@ -79,19 +90,22 @@ public class AnotherSimpleTest implements Game{
 				xPos = 100;
 				yPos = 100;
 			}
+			if(input.isKeyDown(Input.KEY_S)){
+				shield = true;
+			}
 			if(input.isMouseButtonDown(input.MOUSE_LEFT_BUTTON)){
 				if(input.getMouseX() < xPos){
 					xPos -= movement;			
 				} else {
 					xPos += movement;
 				}
-				
+
 				if(input.getMouseY() < yPos){
 					yPos -= movement;
 				} else {
 					yPos += movement;
 				}
-				
+
 			}
 			if(input.isKeyDown(Input.KEY_ENTER)){
 				dead = false;
@@ -118,35 +132,64 @@ public class AnotherSimpleTest implements Game{
 				xPos = 0;
 			} else if(xPos < 1){
 				xPos = xMax;
-			}
-			for(int h = 0; h < 20; h++){
-				for(int i = 0; i < 20; i++){
-					if(yBossPositions[h] == yPositions[i]){
-						for(int j = 0; h < 20; h++){
-							for(int k = 0; i < 20; i++){
-								if(xBossPositions[j] == xPositions[k]){
-									play = "You died(Enter to restart)";
-									dead = true;
+			}if(!shield){
+				for(int h = 0; h < 20; h++){
+					for(int i = 0; i < 20; i++){
+						if(yBossPositions[h] == yPositions[i]){
+							for(int j = 0; h < 20; h++){
+								for(int k = 0; i < 20; i++){
+									if(xBossPositions[j] == xPositions[k]){
+										play = "You died(Enter to restart)";
+										dead = true;
+									}
+								}
+							}
+						}
+					}
+				}
+				for(int h = 0; h < 20; h++){
+					for(int i = 0; i < 20; i++){
+						if(xBoss2Positions[h] == xPositions[i]){
+							for(int j = 0; h < 20; h++){
+								for(int k = 0; i < 20; i++){
+									if(yBoss2Positions[j] == yPositions[k]){
+										play = "You died(Enter to restart)";
+										dead = true;
+									}
 								}
 							}
 						}
 					}
 				}
 			}
-			for(int h = 0; h < 20; h++){
-				for(int i = 0; i < 20; i++){
-					if(xBoss2Positions[h] == xPositions[i]){
-						for(int j = 0; h < 20; h++){
-							for(int k = 0; i < 20; i++){
-								if(yBoss2Positions[j] == yPositions[k]){
-									play = "You died(Enter to restart)";
-									dead = true;
+			else{
+				for(int h = 0; h < 20; h++){
+					for(int i = 0; i < 20; i++){
+						if(yBossPositions[h] == yShieldPositions[i]){
+							for(int j = 0; h < 20; h++){
+								for(int k = 0; i < 20; i++){
+									if(xBossPositions[j] == xShieldPositions[k]){
+										shield = !shield;
+									}
 								}
 							}
 						}
 					}
 				}
-			}
+				for(int h = 0; h < 20; h++){
+					for(int i = 0; i < 20; i++){
+						if(xBoss2Positions[h] == xPositions[i]){
+							for(int j = 0; h < 20; h++){
+								for(int k = 0; i < 20; i++){
+									if(yBoss2Positions[j] == yPositions[k]){
+										shield = !shield;
+									}
+								}
+							}
+						}
+					}
+				}
+			}	
 		}
 	}
 
@@ -157,9 +200,12 @@ public class AnotherSimpleTest implements Game{
 		if(!dead)
 			g.drawRect(xPos, yPos, size, size);
 		g.setColor(Color.red);
-		g.drawOval(xBossPos, yBossPos, 20, 20, 20);
+		g.drawOval(xBossPos, yBossPos, 20, 20);
 		g.drawOval(xBossPos, yBossPos, 20, 20);
 		g.drawOval(xBoss2Pos, yBoss2Pos, 20, 20);
+		g.setColor(Color.blue);
+		if(shield)
+		g.drawOval(xPos, yPos, 20, 20);
 
 	}
 
