8d12929a4c14d3a15f69d11b75547bd546b2d249
Bosch <magnushuttu@gmail.com>
2012-03-15 15:58:34 +0100

    Måste committa för att synca <.<

---
+++ src/cannontetris/test/TestProject.java
@@ -0,0 +1,19 @@
+package cannontetris.test;
+
+import cannontetris.model.*;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.SlickException;
+
+public class TestProject {
+	
+	
+	public static void main(String[] args){
+		try {
+			AppGameContainer app = new AppGameContainer(new GamePlayState());
+			app.start();
+			app.setShowFPS(false);
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
+	}
+}
