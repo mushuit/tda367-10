a9840cde21cb790760f2c9451034c12fa6ec8f9e
Andreas Karlberg <andkarlb@student.chalmers.se>
2012-03-22 15:09:17 +0100

    Cannon movement

--- src/tetrix/core/Cannon.java
+++ src/tetrix/core/Cannon.java
@@ -7,11 +7,15 @@ public class Cannon extends Image {
 	private float yPos;
 	private Image cannonImage;
 	private Track track;
+	private int value;
+	private int rotation;
 	
 	public Cannon(float xPos, float yPos, Image cannonImage) {
 		super(cannonImage);
 		this.xPos = xPos;
 		this.yPos = yPos;
+		rotation = 0;
+		value = 200;
 	}
 	
 	public void setImage(Image cannonImage) {
@@ -38,6 +42,25 @@ public class Cannon extends Image {
 	public void move(float x, float y) {
 		this.xPos += x;
 		this.yPos += y;
+		value += x+(-y);
+		if (x>=0 && x<=400) {
+			rotation = 0;
+		}
+		else if (x>400 && x<=900) {
+			rotation = 90;
+		}
+		else if (x>900 && x<=1400) {
+			rotation = 180;
+		}
+		else { 
+			rotation = 240;
+		
+		}
 	}
+	
+	public float getRotation() {
+		return rotation;
+	}
+	
 	
 }
--- test/tetrix/core/TestBullets.java
+++ test/tetrix/core/TestBullets.java
@@ -103,11 +103,8 @@ public class TestBullets implements Game{
 	}
 
 	public void render(GameContainer arg0, Graphics g) throws SlickException {
-<<<<<<< HEAD
-		
-=======
+
 		g.setBackground(Color.white);
->>>>>>> Bullets fönkar
 		g.setColor(Color.white);
 		for(int i = 0; i < bullets; i++)
 			if(!power && shot)
