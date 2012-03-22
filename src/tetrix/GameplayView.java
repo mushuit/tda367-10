c4030fb60a53eb59cd7b45fb674a4d2bcdfce044
Mushu <linus@mushu.it>
2012-03-22 16:41:38 +0100

    Mindre ändringar för att underlätta testning

--- src/tetrix/GameplayView.java
+++ src/tetrix/GameplayView.java
@@ -2,13 +2,22 @@ package tetrix;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
+import tetrix.core.Cannon;
+
 public class GameplayView extends BasicGameState {
 
 	private int stateID;
+	
+	private Image background;
+	private Image cannonImage;
+	
+	private Cannon cannon;
 	
 	public GameplayView(int stateID) {
 		this.stateID = stateID;
@@ -17,21 +26,31 @@ public class GameplayView extends BasicGameState {
 	@Override
 	public void init(GameContainer arg0, StateBasedGame arg1)
 			throws SlickException {
-		// TODO Auto-generated method stub
-		
+		background= new Image("img/game-background.png");
+		cannonImage = new Image("img/cannon2.png");
+		cannon = new Cannon(200,500);
 	}
 
 	@Override
 	public void render(GameContainer arg0, StateBasedGame arg1, Graphics arg2)
 			throws SlickException {
-		// TODO Auto-generated method stub
-		
+		background.draw(0,0);
+		cannonImage.draw(cannon.getX(), cannon.getY());	
 	}
 
 	@Override
-	public void update(GameContainer arg0, StateBasedGame arg1, int arg2)
+	public void update(GameContainer gc, StateBasedGame arg1, int arg2)
 			throws SlickException {
-		// TODO Auto-generated method stub
+		Input input = gc.getInput();
+		
+		if(input.isKeyDown(Input.KEY_RIGHT)) {
+			cannon.move(1,0);
+		}
+		
+		if(input.isKeyDown(Input.KEY_LEFT)) {
+			cannon.move(-1,0);
+		}
+		cannonImage.setRotation(cannon.getRotation());
 		
 	}
 
