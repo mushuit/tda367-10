388bf5aa2adfdc693f533e1e12e43a843d972db4
Mushu <linus@mushu.it>
2012-03-22 14:17:44 +0100

    Nya klasser

---
+++ src/tetrix/HighscoreView.java
@@ -0,0 +1,43 @@
+package tetrix;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class HighscoreView extends BasicGameState {
+
+	private int stateID;
+	
+	public HighscoreView(int stateID) {
+		this.stateID = stateID;
+	}
+	
+	@Override
+	public void init(GameContainer arg0, StateBasedGame arg1)
+			throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(GameContainer arg0, StateBasedGame arg1, Graphics arg2)
+			throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(GameContainer arg0, StateBasedGame arg1, int arg2)
+			throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getID() {
+		return stateID;
+	}
+
+}
