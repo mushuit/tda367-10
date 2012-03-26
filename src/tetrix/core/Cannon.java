a5ac8658bc9b3266a563c50509a9e51beea44f49
Mushu <linus@mushu.it>
2012-03-26 21:22:02 +0200

    Nu funkar rotation

--- src/tetrix/core/Cannon.java
+++ src/tetrix/core/Cannon.java
@@ -12,7 +12,7 @@ public class Cannon{
 		this.xPos = xPos;
 		this.yPos = yPos;
 		rotation = 0;
-		value = xPos;
+		value = xPos - 25;
 	}
 	
 	public float getX() {
@@ -34,32 +34,59 @@ public class Cannon{
 	
 	public void move(float direction) {
 		value += direction;
-		
-		if (value>=0 && value<425) {
-			rotation = 0;
-			this.xPos += direction;
+		if(direction > 0){
+			if (value>=0 && value<=400) {
+				rotation = 0;
+				this.xPos += direction;
+			}
+			else if (value>400 && value<=900) {
+				rotation = -90;
+				this.yPos -= direction;
+			}
+			else if (value>900 && value<=1300) {
+				rotation = 180;
+				this.xPos -= direction;
+			}
+			else if (value>1300 && value<=1800){ 
+				rotation = 90;
+				this.yPos += direction;
+			}
+
+			if(value > 1800) {
+				value = 0;
+			}
+			if(value < 0) {
+				value = 1800;
+			}
 		}
-		else if (value>=425 && value<900) {
-			rotation = -90;
-			this.yPos -= direction;
-		}
-		else if (value>=900 && value<1200) {
-			rotation = 180;
-			this.xPos -= direction;
-		}
-		else if (value>1200 && value<=1800){ 
-			rotation = 90;
-			this.yPos += direction;
-		}
-		
-		if(value > 1800) {
-			value = 0;
+		else{
+			if (value>=0 && value<400) {
+				rotation = 0;
+				this.xPos += direction;
+			}
+			else if (value>=400 && value<900) {
+				rotation = -90;
+				this.yPos -= direction;
+			}
+			else if (value>=900 && value<1300) {
+				rotation = 180;
+				this.xPos -= direction;
+			}
+			else if (value>=1300 && value<=1800){ 
+				rotation = 90;
+				this.yPos += direction;
+			}
+
+			if(value >= 1800) {
+				value = 0;
+			}
+			if(value <= 0) {
+				value = 1800;
+			}
 		}
 	}
 	
 	public int getRotation() {
 		return rotation;
-	}
-	
-	
+	}	
 }
