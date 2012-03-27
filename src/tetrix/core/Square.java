82ca960a56d694df6640a7b60e31aa2d74ce7132
Mushu <linus@mushu.it>
2012-03-27 17:07:58 +0200

    Gjort om lite

--- src/tetrix/core/Square.java
+++ src/tetrix/core/Square.java
@@ -1,11 +1,15 @@
 package tetrix.core;
 
-public class Square {
+import org.newdawn.slick.geom.Rectangle;
+
+public class Square extends Rectangle {
 	
+	public Square(float x, float y, float width, float height) {
+		super(x, y, width, height);
+		// TODO Auto-generated constructor stub
+	}
+
 	private int size;
 	
-	public Square(int size) {
-		
-		this.size = size;
-	}
+
 }
