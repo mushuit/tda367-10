2a24f4f6cc5ac0eca0617e95684bb28e8020077d
Andreas Karlberg <andkarlb@student.chalmers.se>
2012-03-28 11:06:18 +0200

    hScore

--- src/tetrix/core/HighScore.java
+++ src/tetrix/core/HighScore.java
@@ -1,5 +1,18 @@
 package tetrix.core;
 
 public class HighScore {
+	private int Score;
+	private String PlayerName;
 
+	public int getScore() {
+		return Score;
+	}
+	public String getName() {
+		return PlayerName;
+	}
+	public HighScore(int Score, String PlayerName){
+		this.Score = Score;
+		this.PlayerName = PlayerName;
+		
+	}
 }
