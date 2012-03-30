c8f71c3477cefaf948f057c3d5698069d4fd5af6
Andreas Karlberg <andkarlb@student.chalmers.se>
2012-03-30 12:19:37 +0200

    Music

--- src/tetrix/sound/GameMusic.java
+++ src/tetrix/sound/GameMusic.java
@@ -1,7 +1,19 @@
 package tetrix.sound;
 
 import org.newdawn.slick.Music;
+import org.newdawn.slick.SlickException;
 
 public class GameMusic {
+	private Music music;
 
+	public GameMusic() throws SlickException{
+		music = new Music("Sound/test.wav");
+	}
+	private void play(){
+		music.play();
+	}
+	private void loop() {
+		music.loop();
+	}
+	
 }
