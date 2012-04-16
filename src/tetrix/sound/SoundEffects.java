d5bee2ae96f894f67342a9f411f2cbac19b17763
Andreas Karlberg <andkarlb@student.chalmers.se>
2012-04-16 10:37:09 +0200

    SFX

--- src/tetrix/sound/SoundEffects.java
+++ src/tetrix/sound/SoundEffects.java
@@ -1,7 +1,18 @@
 package tetrix.sound;
 
+import org.newdawn.slick.SlickException;
 import org.newdawn.slick.Sound;
 
 public class SoundEffects {
+	private Sound sfx;
 
+	public SoundEffects() throws SlickException{
+		sfx = new Sound("Sound/test.waw");
+	}
+	private void play(){
+		sfx.play();
+		}
+	private void loop(){
+		sfx.loop();
+	}
 }
