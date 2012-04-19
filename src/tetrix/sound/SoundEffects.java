403ca40de84d040faef15d8fe3ab9178dff86155
Andreas Karlberg <andkarlb@student.chalmers.se>
2012-04-19 15:18:23 +0200

    Improved SoundClasses

--- src/tetrix/sound/SoundEffects.java
+++ src/tetrix/sound/SoundEffects.java
@@ -4,23 +4,95 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.Sound;
 
 public class SoundEffects {
-	public static Sound sfx;
+	private static Sound sfxShot;
+	private static Sound sfxBlowUp;
+	private static Sound sfxStartGame;
+	private static Sound sfxMenuClick;
+	private static Sound sfxHighScore;
+	private static Sound sfxDeath;
 
 	public SoundEffects() throws SlickException{
-		sfx = new Sound("Sound/test.wav");
-		play();
+		sfxShot = new Sound("Sound/music.wav");		// Sound of the bullet when you shoot
+		sfxBlowUp = new Sound("Sound/test.wav");	// Sound of a block blown up
+		sfxStartGame = new Sound("Sound/test.wav");	// Sound when you start the game 
+		sfxMenuClick = new Sound("Sound/test.wav");	// Sound of a menu click
+		sfxHighScore = new Sound("Sound/test.wav");	// Sound when you break a new High Score
+		sfxDeath = new Sound("Sound/test.wav");		// Sound when it is Game Over
 	}
-	public static void play(){
-		System.out.println("Ljud OK");
-		sfx.play();
-		
+
+	public void shotPlay(){
+		sfxShot.play();
 	}
-	private void stop(){
-		sfx.stop();
+
+	public void shotLoop(){
+		sfxShot.loop();
 	}
-	private void loop(){
-		sfx.loop();
+
+	public void shotStop(){
+		sfxShot.stop();
 	}
+
+	public void blowUpPlay(){
+		sfxBlowUp.play();
+	}
+
+	public void blowUpLoop(){
+		sfxBlowUp.loop();
+	}
+
+	public void blowUpStop(){
+		sfxBlowUp.stop();
+	}
+
+	public void startGamePlay(){
+		sfxStartGame.play();
+	}
+
+	public void startGameLoop(){
+		sfxStartGame.loop();
+	}
+
+	public void startGameStop(){
+		sfxStartGame.stop();
+	}
+
+	public void menuClickPlay(){
+		sfxMenuClick.play();
+	}
+
+	public void menuClickLoop(){
+		sfxMenuClick.loop();
+	}
+
+	public void menuClickStop(){
+		sfxMenuClick.stop();
+	}
+
+	public void highScorePlay(){
+		sfxHighScore.play();
+	}
+
+	public void highScoreLoop(){
+		sfxHighScore.loop();
+	}
+
+	public void highScoreStop(){
+		sfxHighScore.stop();
+	}
+
+	public void deathPlay(){
+		sfxDeath.play();
+	}
+
+	public void deathLoop(){
+		sfxDeath.loop();
+	}
+
+	public void deathStop(){
+		sfxDeath.stop();
+	}
+
+
 	public static void main(String[]args) throws SlickException{
 		SoundEffects se = new SoundEffects();
 	}
