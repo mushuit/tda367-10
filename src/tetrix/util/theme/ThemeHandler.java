88419bc81950ff3969792c799955425500d5c1d2
Kara <jonathankara@hotmail.com>
2012-05-10 14:39:58 +0200

    same as before

---
+++ src/tetrix/util/theme/ThemeHandler.java
@@ -0,0 +1,35 @@
+package tetrix.util.theme;
+
+import org.newdawn.slick.Image;
+
+public class ThemeHandler {
+
+	private String theme;
+	public static final String BACKGROUND_IMG = "background.png";
+	public static final String TETRIX_LOGO_IMG = "tetrix_logo.png";
+	public static final String BACK_IMG = "back.png";
+	public static final String EFFECT_IMG = "effect.png";
+	public static final String EXIT_IMG = "exit.png";
+	public static final String GAME_BACKGROUND_IMG = "game_background.png";
+	public static final String HIGHSCORE_IMG = "highscore.png";
+	public static final String HOVER_IMG = "menu_hover.png";
+	public static final String HOVER_SMALL_IMG = "hover_small.png";
+	public static final String MAIN_MENU_IMG = "main_menu.png";
+	public static final String MUSIC_IMG = "music.png";
+	public static final String NEW_GAME_IMG = "new_game.png";
+	
+	
+	public void setTheme(String theme){
+		this.theme = theme;
+	}
+	public String getTheme(){
+		return theme;
+	}
+	
+	
+//	public Image getImage(String name){
+//		return 
+//	}
+	
+	
+}
