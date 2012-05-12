9ea9615455bca729640351556cdaddf3ef5ef816
Bosch <magnushuttu@gmail.com>
2012-05-12 17:02:22 +0200

    The O block is complete

--- src/tetrix/core/tetrominos/J.java
+++ src/tetrix/core/tetrominos/J.java
@@ -13,7 +13,12 @@ import tetrix.core.Position;
 import tetrix.util.Util;
 
 public class J extends Tetromino{
-	
+	private boolean SqrDstr;
+	private boolean SqrDstr2;
+	private boolean SqrDstr3;
+	private boolean SqrDstr4;
+	private boolean used;
+
 	public J(int startX, BlockBox bBox){
 		this(startX, (Util.WINDOW_WIDTH - Util.BOX_WIDTH)/2, bBox);
 	}
@@ -21,9 +26,14 @@ public class J extends Tetromino{
 	public J(int startX, int leftIn, BlockBox bBox){
 		this(startX, leftIn, Util.SQUARE_SIZE, bBox);
 	}
-	
+
 	public J(int startX, int leftIn, int fallspeed, BlockBox bBox){
 		super(startX, leftIn,fallspeed, bBox);
+		SqrDstr = false;
+		SqrDstr2 = false;
+		SqrDstr3 = false;
+		SqrDstr4 = false; 
+		used = false;
 	}
 
 
@@ -43,16 +53,54 @@ public class J extends Tetromino{
 	}
 
 	public void notWhole() throws SlickException{
+		System.out.println("1:" + SqrDstr + "  2:" + SqrDstr2 + "  3:" + SqrDstr3 + "  4:" + SqrDstr4);
 		Square[] sq2 = getSquares();
 		for(Square s : getSquares()){
 			if(s.destroyed()){
-				if(s.getNbr() == 1){
-					sq2[0].destroy();
-					bBox.newBrokenBlock(0, this, sq2[0].getPos(), getX());
+				if(!s.used()){
+					if(s.getNbr() == 0){
+
+						SqrDstr = true;
+					}
+					else if(s.getNbr() == 1){
+						if(SqrDstr3 && !used && !SqrDstr4 && !SqrDstr){
+							sq2[0].destroy();
+							sq2[0].use();
+							bBox.newBrokenBlock(0, this, sq2[0].getPos(), getX());
+							System.out.println("clonad()");
+							used = true;
+						} 
+						SqrDstr2 = true;
+					}
+					else if(s.getNbr() == 2){
+						if(SqrDstr2 && !used && !SqrDstr4 && !SqrDstr){
+							sq2[3].destroy();
+							sq2[3].use();
+							bBox.newBrokenBlock(3, this, sq2[3].getPos(), getX());
+							System.out.println("clonad()");
+							used = true;
+						}
+						SqrDstr3 = true;
+					}
+					else if(s.getNbr() == 3){
+						SqrDstr4 = true;
+					}
 				}
-				else if(s.getNbr() == 2){
-					sq2[3].destroy();
-					bBox.newBrokenBlock(3, this, sq2[3].getPos(), getX());
+			}
+		}
+		for(Square s : getSquares()){
+			if(s.destroyed()){
+				if(!s.used()){
+					if(s.getNbr() == 1){
+						sq2[0].destroy();
+						sq2[0].use();
+						bBox.newBrokenBlock(0, this, sq2[0].getPos(), getX());
+					}
+					else if(s.getNbr() == 2){
+						sq2[3].destroy();
+						sq2[3].destroy();
+						bBox.newBrokenBlock(3, this, sq2[3].getPos(), getX());
+					}
 				}
 			}
 		}
--- src/tetrix/core/tetrominos/O.java
+++ src/tetrix/core/tetrominos/O.java
@@ -59,7 +59,7 @@ public class O extends Tetromino{
 			if(s.destroyed()){
 				if(!s.used()){
 					if(s.getNbr() == 0){
-						if(SqrDstr4 && !used){
+						if(SqrDstr4 && !used && !SqrDstr2 && !SqrDstr3){
 							sq2[1].destroy();
 							sq2[1].use();
 							bBox.newBrokenBlock(1, this, sq2[1].getPos(), getX());
@@ -69,7 +69,7 @@ public class O extends Tetromino{
 						SqrDstr = true;
 					}
 					else if(s.getNbr() == 1){
-						if(SqrDstr3 && !used){
+						if(SqrDstr3 && !used && !SqrDstr4 && !SqrDstr){
 							sq2[0].destroy();
 							sq2[0].use();
 							bBox.newBrokenBlock(0, this, sq2[0].getPos(), getX());
@@ -79,7 +79,7 @@ public class O extends Tetromino{
 						SqrDstr2 = true;
 					}
 					else if(s.getNbr() == 2){
-						if(SqrDstr2 && !used){
+						if(SqrDstr2 && !used && !SqrDstr4 && !SqrDstr){
 							sq2[3].destroy();
 							sq2[3].use();
 							bBox.newBrokenBlock(3, this, sq2[3].getPos(), getX());
@@ -89,7 +89,7 @@ public class O extends Tetromino{
 						SqrDstr3 = true;
 					}
 					else if(s.getNbr() == 3){
-						if(SqrDstr && !used){
+						if(SqrDstr && !used && !SqrDstr3 && !SqrDstr2){
 							sq2[2].destroy();
 							sq2[2].use();
 							bBox.newBrokenBlock(2, this, sq2[2].getPos(), getX());
